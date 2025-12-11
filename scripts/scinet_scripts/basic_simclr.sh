6dd03d958451764ba405bccf1d302b963e7c702f
skostousov <samuel.kostousov@gmail.com>
2025-12-10 22:48:11 -0500

    updated script

--- scripts/scinet_scripts/basic_simclr.sh
+++ scripts/scinet_scripts/basic_simclr.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #SBATCH --job-name=simclr_basic
-#SBATCH --output=/scratch/CompNeuro/job_logs/%x_%j.out
-#SBATCH --error=/scratch/CompNeuro/job_logs/%x_%j.err
 #SBATCH --time=00:30:00
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
@@ -23,3 +21,6 @@ module load opencv/4.11.0
 python -u "CompNeuro/Computational_Neuroscience_-25--26/scripts/train_simclr.py" --imagenet_root CompNeuro/Computational_Neuroscience_-25--26/split_data --batch_size 32 --epochs 1 --save_dir new_logs --skip_knn_metric True --skip_spectrum_metric True --spectral_loss_coeff 0.1 --neural_ev True --limit_train 32 --limit_val 32
 
 echo "train_simclr.py launched"
+
+# SBATCH --output=/scratch/CompNeuro/job_logs/%x_%j.out
+# SBATCH --error=/scratch/CompNeuro/job_logs/%x_%j.err
