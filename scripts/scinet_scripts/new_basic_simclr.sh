810c50cc44a0303c3fb65a7f46ba231bab3b7749
skostousov <samuel.kostousov@gmail.com>
2025-12-11 22:40:35 -0500

    new simclr script

--- scripts/scinet_scripts/new_basic_simclr.sh
+++ scripts/scinet_scripts/new_basic_simclr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --time 02:00:00
+#SBATCH --time 24:00:00
 #SBATCH --output=basic_simclr_output_%j.txt
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
