69347b6abd1303a6e0b7705c1c2d355bbe8dc074
skostousov <samuel.kostousov@gmail.com>
2025-12-11 21:57:36 -0500

    new simclr script

---
+++ scripts/scinet_scripts/new_basic_simclr.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=192
+#SBATCH --time 02:00:00
+#SBATCH --output=python_output_%j.txt
+#SBATCH --mail-type=BEGIN,END,FAIL,ALL
+#SBATCH --mail-user=samuel.kostousov@gmail.com
+
+# Load required modules
+module load postgresql/16.0
+module load StdEnv/2023  gcc/12.3  cuda/12.2
+module load mpi4py/3.1.4
+module load opencv/4.11.0
+
+# Activate virtual environment if needed
+source $HOME/CompNeuro/Computational_Neuroscience_-25--26/.venv/bin/activate
+export PYTHONPATH=$PWD
+
+
+# Run the Python script
+python -u "CompNeuro/Computational_Neuroscience_-25--26/scripts/train_simclr.py" --imagenet_root CompNeuro/Computational_Neuroscience_-25--26/split_data --batch_size 32 --epochs 1 --save_dir new_logs --skip_knn_metric True --skip_spectrum_metric True --spectral_loss_coeff 0.1 --neural_ev True --limit_train 32 --limit_val 32
