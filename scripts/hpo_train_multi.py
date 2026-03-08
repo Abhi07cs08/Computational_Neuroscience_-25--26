564adb016b71911e0e6ba296d71f1591a381d125
skostousov <samuel.kostousov@gmail.com>
2026-03-07 21:44:18 -0500

    changed grid search to random sampler

--- scripts/hpo_train_multi.py
+++ scripts/hpo_train_multi.py
@@ -11,7 +11,7 @@ ap.add_argument("--optuna_db", type=str, default="optuna.db")
 ap.add_argument("--tune_spectral_loss_coeff", action="store_true")
 ap.add_argument("--tune_target_alpha", action="store_true")
 ap.add_argument("--tune_temperature", action="store_true")
-ap.add_argument("--grid_search", action="store_true")
+ap.add_argument("--random_sampler", action="store_true")
 
 args = parse_args(ap=ap)
 
@@ -25,7 +25,7 @@ if args.grid_search:
         study_name=args.optuna_study_name,
         load_if_exists=True,
         directions=['maximize', 'maximize'],
-        sampler=optuna.samplers.GridSampler(search_space)
+        sampler=optuna.samplers.RandomSampler(search_space)
     )
 else:
         study = optuna.create_study(
@@ -38,14 +38,14 @@ else:
 def objective(trial):
     # spectral_loss_warmup_epochs = trial.suggest_int("spectral_loss_warmup_epochs", 0, 20, step=5)
     if args.tune_temperature:
-        if args.grid_search:
+        if args.random_sampler:
             args.tau = trial.suggest_categorical("tau", search_space["tau"])
         else:
             args.tau = trial.suggest_float("tau", 0.05, 0.5, step=0.05)
         args.tag = "tuning_temperature"
         print(f"tau: {args.tau}")
     if args.tune_spectral_loss_coeff:
-        if args.grid_search:
+        if args.random_sampler:
             args.spectral_loss_coeff = trial.suggest_categorical("spectral_loss_coeff", search_space["spectral_loss_coeff"])
         else:
             args.spectral_loss_coeff = trial.suggest_float("spectral_loss_coeff", 0.0, 2.0, step=0.05)
@@ -54,7 +54,7 @@ def objective(trial):
     if args.tune_target_alpha:
         assert args.skip_alpha == False, "Cannot tune target_alpha if skip_alpha is True"
         assert args.spectral_loss_coeff != 0.0, "Cannot tune target_alpha if spectral_loss_coeff is 0.0"
-        if args.grid_search:
+        if args.random_sampler:
             args.target_alpha = trial.suggest_categorical("target_alpha", search_space["target_alpha"])
         else:
             args.target_alpha = trial.suggest_float("target_alpha", 0.0, 2.0, step=0.05)
