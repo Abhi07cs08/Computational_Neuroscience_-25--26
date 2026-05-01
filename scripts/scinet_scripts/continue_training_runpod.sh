b79f5e860596893bc926abc1330a48f0bc7028e2
skostousov <samuel.kostousov@gmail.com>
2026-05-01 14:55:13 -0400

    runpod resume

---
+++ scripts/scinet_scripts/continue_training_runpod.sh
@@ -0,0 +1,12 @@
+
+source /workspace/Computational_Neuroscience_-25--26/.venv/bin/activate
+export PYTHONPATH=$PWD
+export PYTHONPATH="/workspace/Computational_Neuroscience_-25--26":$PYTHONPATH
+
+ckpt_path="${1-/scratch/kostouso/CompNeuro/Computational_Neuroscience_-25--26/all_model_logs/sk_logs_spec_loss_0.0/start_20260101-221806/simclr/last.pt}"
+more_epochs="${2-1}"
+
+echo "Arguments: more_epochs=$more_epochs ckpt_path=$ckpt_path"
+
+# Run the Python script
+python -u "/workspace/Computational_Neuroscience_-25--26/scripts/train_simclr.py" --imagenet_root "/workspace/Computational_Neuroscience_-25--26/imagenet" ckpt --ckpt_path $ckpt_path --more_epochs $more_epochs
